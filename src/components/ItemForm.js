61bc3bb65b2bc89b5c87cc4b3fbc31032a2b31a4
shannone <shannone426@gmail.com>
2022-09-22 15:28:38 -0400

    onSearch added

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("Produce");
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleCategoryChange(event) {
+    setCategory(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+   const newObj=({
+      id: uuid(),
+      name,
+      category,
+    });
+    onItemFormSubmit(newObj)
+    console.log(newObj)
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleNameChange}
+        />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select
+          name="category"
+          value={category}
+          onChange={handleCategoryChange}
+        >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
@@ -23,4 +54,60 @@ function ItemForm(props) {
   );
 }
 
+
+
 export default ItemForm;
+
+// function ItemForm({onItemFormSubmit}) {
+//   const [formData, setFormData] = useState({
+//         name: "",
+//         category: "Produce",
+//       });
+    
+//       function handleChange(event) {
+//         setFormData({
+//           ...formData,
+//           [event.target.name]: event.target.value,
+//         });
+//       }
+    
+//       function handleSubmit(event) {
+//         event.preventDefault();
+//         onItemFormSubmit({
+//           id: uuid(),
+//           ...formData
+//         });
+//       }
+    
+//       return (
+//         <form className="NewItem" onSubmit={handleSubmit}>
+//           <label>
+//             Name:
+//             <input
+//               type="text"
+//               name="name"
+//               value={formData.name}
+//               onChange={handleChange}
+//             />
+//           </label>
+    
+//           <label>
+//             Category:
+//             <select
+//               name="category"
+//               value={formData.category}
+//               onChange={handleChange}
+//             >
+//               <option value="Produce">Produce</option>
+//               <option value="Dairy">Dairy</option>
+//               <option value="Dessert">Dessert</option>
+//             </select>
+//           </label>
+    
+//           <button type="submit">Add to List</button>
+//         </form>
+//       );
+//     }
+
+
+// export default ItemForm;
